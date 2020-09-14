2cc2d55854a679abfa4aee15db9ef927ecefb774
happiness <melow2@naver.com>
2020-09-14 09:00:00 +0900

    [DEV] Retrofit Instance

---
+++ app/src/main/java/com/khs/retrofit/RetrofitInstance.kt
@@ -0,0 +1,18 @@
+package com.khs.retrofit
+
+import com.google.gson.GsonBuilder
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetrofitInstance {
+    companion object {
+        private val BASE_URL = "https://jsonplaceholder.typicode.com/"
+        fun getInstance(): Retrofit? {
+            return Retrofit
+                .Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+                .build()
+        }
+    }
+}
